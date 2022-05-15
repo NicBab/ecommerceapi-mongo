c0be0a68912df828115c262c158ac156b5566b38
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2022-05-15 14:43:05 -0500

    register & login routes using cryptoJS

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
-const userRouter = require("./routes/users")
+const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
 
 //connect to mongoose database
 mongoose
@@ -11,8 +12,16 @@ mongoose
   .then(() => console.log("DB connection successful"))
   .catch(() => console.log("error connecting to DB"));
 
-app.use("/api/users", userRouter)
 app.use(express.json());
+app.use("/api/users", userRoute)
+app.use("/api/auth", authRoute)
+
+
+
+
+
+
+
 
 //connect to server
 app.listen(process.env.PORT || 3000, () => {
